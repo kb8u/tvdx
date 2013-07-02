903e45e89a90a2cc287d301a288e1c9009dfcd1a
Russell Dwarshuis <kb8u_vhf@hotmail.com>
2013-07-01 23:30:40 -0400

    queries tsid via url (slow)

--- lib/tvdx/Controller/RawSpot.pm
+++ lib/tvdx/Controller/RawSpot.pm
@@ -9,6 +9,8 @@ use RRDs;
 use List::MoreUtils 'none';
 use Data::Dumper;
 
+# URL for looking up callsign, location, TSID, etc.
+my $RABBITEARS_TVQ = "http://www.rabbitears.info/rawlookup.php?";
 
 BEGIN { extends 'Catalyst::Controller::REST' }
 
@@ -58,26 +60,26 @@ sub raw_spot_POST :Global {
   }
 
   RAWSPOT: foreach my $channel (keys %{$json->{'rf_channel'}}) {
-    my $raw_channel       = $json->{'rf_channel'}->{$channel};
-    my $modulation        = $raw_channel->{modulation};
-    my $strength          = $raw_channel->{strength};
-    my $sig_noise         = $raw_channel->{sig_noise};
-    my $symbol_err        = $raw_channel->{symbol_err};
-    my $tsid              = $raw_channel->{tsid};
-    my %virtual           = $raw_channel->{virtual};
-    my $reporter_callsign = $raw_channel->{reporter_callsign};
-$c->log->debug("channel: $channel strength: $strength");
-next RAWSPOT;
-BUG: # subroutines to be written....
+    my $channel_details       = $json->{'rf_channel'}->{$channel};
+    my $modulation        = $channel_details->{modulation};
+    my $strength          = $channel_details->{strength};
+    my $sig_noise         = $channel_details->{sig_noise};
+    my $symbol_err        = $channel_details->{symbol_err};
+    my $tsid              = $channel_details->{tsid};
+    my %virtual           = %{$channel_details->{virtual}};
+    my $reporter_callsign = $channel_details->{reporter_callsign};
+$c->log->debug("#####channel: $channel strength: $strength");
 
     # return callsign or undef if it can't be determined and a virtual
     # channel for legacy column in fcc table
-    my ($callsign,$fcc_virtual) = _find_call($raw_channel);
-    $c->log->debug("channel $channel:found callsign: $callsign\n");
-
+    my ($callsign,$fcc_virtual) = _find_call($channel_details,$channel);
+if (defined $callsign) {
+    $c->log->debug("#####channel $channel:found callsign: $callsign\n");
+}
+next RAWSPOT;
     # record signal strength and possibly sig_noise if no call was found
     if (! defined $callsign) {
-      _rrd_update_nocall($raw_channel);
+      _rrd_update_nocall($channel_details);
       next RAWSPOT;
     }
 
@@ -86,15 +88,15 @@ BUG: # subroutines to be written....
       next RAWSPOT;
     }
     # add or update virtual channel table
-    if (! $self->_virtual_current($c,$callsign,$raw_channel)) {
+    if (! $self->_virtual_current($c,$callsign,$channel_details)) {
       next RAWSPOT;
     }
     # add or update tsid table if needed
-    if (! $self->_tsid_current($c,$callsign,$raw_channel)) {
+    if (! $self->_tsid_current($c,$callsign,$channel_details)) {
       next RAWSPOT;
     }
     # update rrd file and Signal table
-    _signal_update($c,$tuner_id,$tuner_number,$callsign,$raw_channel);
+    _signal_update($c,$tuner_id,$tuner_number,$callsign,$channel_details);
   }
 
   $c->response->body('OK');
@@ -102,18 +104,74 @@ BUG: # subroutines to be written....
 }
 
 
+# determine callsign from raw channel information
+sub _find_call {
+  my ($channel_details,$tuner_channel) = @_;
+  my ($call,$fcc_channel,
+      $city,$state,
+      $rcamsl,$erp,
+      $n_or_s,$lat_deg,$lat_min,$lat_sec,
+      $w_or_e,$lon_deg,$lon_min,$lon_sec,
+      $digital_tsid,$analog_tsid,$observed_tsid);
+
+  # try tsid (excepting 0, 1 and greater than 32766) and channel
+  if (   $channel_details->{tsid} 
+      && $channel_details->{tsid} > 1
+      && $channel_details->{tsid} < 32767) {
+######## this works, but is very slow and hammers on rabbitears.
+######## try cacheing it, or use DBIx to query directly
+    my $rlu = get($RABBITEARS_TVQ . "tsid=$channel_details->{tsid}");
+    if (defined $rlu) {
+      foreach my $s (split /\n/, $rlu) {
+        my ($s_call,$s_fcc_channel,
+            $s_city,$s_state,
+            $s_rcamsl,$s_erp,
+            $s_n_or_s,$s_lat_deg,$s_lat_min,$s_lat_sec,
+            $s_w_or_e,$s_lon_deg,$s_lon_min,$s_lon_sec,
+            $s_digital_tsid,$s_analog_tsid,$s_observed_tsid) = split /\s*\|/,$s;
+        if ($tuner_channel == $s_fcc_channel) {
+          ($call,$fcc_channel,
+           $city,$state,
+           $rcamsl,$erp,
+           $n_or_s,$lat_deg,$lat_min,$lat_sec,
+           $w_or_e,$lon_deg,$lon_min,$lon_sec,
+           $digital_tsid,$analog_tsid,$observed_tsid)
+          =
+          ($s_call,$s_fcc_channel,
+           $s_city,$s_state,
+           $s_rcamsl,$s_erp,
+           $s_n_or_s,$s_lat_deg,$s_lat_min,$s_lat_sec,
+           $s_w_or_e,$s_lon_deg,$s_lon_min,$s_lon_sec,
+           $s_digital_tsid,$s_analog_tsid,$s_observed_tsid);
+          last;
+        }
+      }
+    }
+  }
+  # else try callsign and channel
+  else {
+  }
+  if (defined $call && defined $fcc_channel) {
+    return ($call,$fcc_channel);
+  }
+  else {
+    return undef
+  }
+}
+
+
 # Check or update (if > 1 day old) or create virtual table entry for callsign
 # Returns 1 on success, 0 on failure.
 sub _virtual_current {
-  my ($self,$c,$callsign,$raw_channel) = @_;
+  my ($self,$c,$callsign,$channel_details) = @_;
 
   my $yesterday = DateTime->from_epoch( 'epoch' => (time() - 86400) );
 
   # process each virtual channel
-  for my $v_channel (keys %{$raw_channel->{virtual}}) {
+  for my $v_channel (keys %{$channel_details->{virtual}}) {
     my ($v_row) = $c->model('DB::Virtual')
                     ->find({'callsign' => $callsign,
-                            'channel' =>$raw_channel->{virtual}->{$v_channel}});
+                            'channel' =>$channel_details->{virtual}->{$v_channel}});
 
     # all new entry?
     if (!$v_row) {
