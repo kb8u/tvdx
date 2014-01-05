41416efce9408e14028cfcce05bd867178d9813b
Russell Dwarshuis <kb8u_vhf@hotmail.com>
2014-01-04 19:24:52 -0500

    displays a (not useful) map using gmap3

--- root/static/one_tuner.js
+++ root/static/one_tuner.js
@@ -1,14 +1,5 @@
-var map;
-function initialize() {
-  var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  map = new google.maps.Map(document.getElementById('one-tuner-map'),
-      mapOptions);
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
+$(document).ready(function() {
+  adjust_height();
+  $('#map-container').gmap3();
+})
 
