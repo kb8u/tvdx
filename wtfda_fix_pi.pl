636eaa175432a2ce9cd6737dd3630890f9208102
Russell Dwarshuis <kb8u_vhf@hotmail.com>
2021-01-31 14:48:20 -0500

    remove debug url, move sub process_res to main

--- wtfda_fix_pi.pl
+++ wtfda_fix_pi.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-#use strict;
+use strict;
 use feature 'say';
 use Getopt::Std;
 use Mojo::UserAgent;
@@ -13,6 +13,7 @@ use lib './lib';
 use tvdx::Model::DB;
 
 
+# debug and help options
 our ($opt_d, $opt_h);
 getopts('dh');
 help() if $opt_h;
@@ -21,9 +22,7 @@ my $fm_fcc_rs = tvdx::Model::DB->new()->resultset('FmFcc');
 
 my $sql_now = DateTime::Format::MySQL->format_datetime(DateTime->now);
 
-#my $site = 'https://picodes.nrscstandards.org/fs_pi_codes_allocated.html';
-my $site = 'https://rabbitears.info/tvdx/root/static/pi.html';
-#my $site = 'https://rabbitears.info/tvdx/root/static/p.html';
+my $site = 'https://picodes.nrscstandards.org/fs_pi_codes_allocated.html';
 my $ua = Mojo::UserAgent->new;
 
 my $res = $ua->get($site)->result;
@@ -37,67 +36,61 @@ unless ($res->is_success)  {
   say "not successsful" if $opt_d;
   exit 1;
 }
-process_res($res);
 
-  
+for my $row ($res->dom->find('tr')->each) {
+  next if $row->at('th');
+  my $td = $row->find('td')->to_array;
+  my $callsign = $td->[0]->at('a')->text;
+  my $pi_code = hex($td->[1]->at('code')->text);
+  my $frequency = $td->[2]->text;
+  my $city_state = $td->[4]->text . ', ' . $td->[5]->text;
+  my $country = 'USA';
+  my $lat_lon = $td->[6]->text;
 
+  $frequency =~ s/\.//;
+  $frequency .= '0000';
 
-sub process_res {
-  my ($res) = @_;
+  $lat_lon =~ s/^\s+//;
+  $lat_lon =~ s/\s+$//;
+  my @lat_lon = split /[^0-9\.NSEW]+/,$lat_lon;
+  my $latitude = $lat_lon[0] + $lat_lon[1]/60 + $lat_lon[2]/3600;
+  $latitude = $lat_lon[3] eq 'S' ? $latitude * -1 : $latitude;
+  my $longitude = $lat_lon[4] + $lat_lon[5]/60 + $lat_lon[6]/3600;
+  $longitude = $lat_lon[7] eq 'W' ? $longitude * -1 : $longitude;
 
-  for my $row ($res->dom->find('tr')->each) {
-    next if $row->at('th');
-    my $td = $row->find('td')->to_array;
-    my $callsign = $td->[0]->at('a')->text;
-    my $pi_code = hex($td->[1]->at('code')->text);
-    my $frequency = $td->[2]->text;
-    my $city_state = $td->[4]->text . ', ' . $td->[5]->text;
-    my $country = 'USA';
-    my $lat_lon = $td->[6]->text;
-
-    $frequency =~ s/\.//;
-    $frequency .= '0000';
-
-    $lat_lon =~ s/^\s+//;
-    $lat_lon =~ s/\s+$//;
-    my @lat_lon = split /[^0-9\.]+/,$lat_lon;
-    my $lattude = $lat_lon[0] + $lat_lon[1]/60 + $lat_lon[2]/3600;
-    my $longitude = -1*($lat_lon[3] + $lat_lon[4]/60 + $lat_lon[5]/3600);
-
-    # create entry in fm_fcc?
-    my ($fcc_row) = $fm_fcc_rs->find({'frequency' => $frequency,
-                                      'callsign' => $callsign,
-                                      'end_date' => undef});
-    if (!defined $fcc_row || $fcc_row == 0) {
-      unless (all {defined $_} ($pi_code,$callsign,$latitude,$longitude,$sql_now,$city_state,$country)) {
-        say ("Missing or bad data in row: ",join ' ',$pi_code,$callsign,$latitude,$longitude,$sql_now,$city_state,$country) if $opt_d;
-        next;
-      }
-      say "creating $callsign" if $opt_d;
-      my $entry = $fm_fcc_rs->create({
-        'callsign' => $callsign,
-        'frequency' => $frequency,
-        'city_state' => $city_state,
-        'country' => $country,
-        'latitude' => $latitude,
-        'longitude' => $longitude,
-        'pi_code' => $pi_code,
-        'start_date' => $sql_now,
-        'last_fcc_lookup' => $sql_now,
-      });
-      if (!$entry) {
-        say ("Couldn't create new fm_fcc row with $pi_code,$callsign,$latitude,$longitude,$sql_now,$city_state,$country") if $opt_d;
-        next;
-      }
+  # create entry in fm_fcc?
+  my ($fcc_row) = $fm_fcc_rs->find({'frequency' => $frequency,
+                                    'callsign' => $callsign,
+                                    'end_date' => undef});
+  if (!defined $fcc_row || $fcc_row == 0) {
+    unless (all {defined $_} ($pi_code,$callsign,$latitude,$longitude,$sql_now,$city_state,$country)) {
+      say ("Missing or bad data in row: ",join ' ',$pi_code,$callsign,$latitude,$longitude,$sql_now,$city_state,$country) if $opt_d;
+      next;
     }
-    # else row exists, just update it if pi_code is different.
-    elsif ($fcc_row->pi_code != $pi_code) {
-      say "$callsign has incorrect pi on wtfda, was ",$fcc_row->pi_code," should be $pi_code" if $opt_d;
-      $fcc_row->update({
-        'pi_code' => $pi_code,
-        'last_fcc_lookup' => $sql_now,
-      });
+    say "creating $callsign" if $opt_d;
+    my $entry = $fm_fcc_rs->create({
+      'callsign' => $callsign,
+      'frequency' => $frequency,
+      'city_state' => $city_state,
+      'country' => $country,
+      'latitude' => $latitude,
+      'longitude' => $longitude,
+      'pi_code' => $pi_code,
+      'start_date' => $sql_now,
+      'last_fcc_lookup' => $sql_now,
+    });
+    if (!$entry) {
+      say ("Couldn't create new fm_fcc row with $pi_code,$callsign,$latitude,$longitude,$sql_now,$city_state,$country") if $opt_d;
+      next;
     }
+  }
+  # else row exists, just update it if pi_code is different.
+  elsif ($fcc_row->pi_code != $pi_code) {
+    say "$callsign has incorrect pi on wtfda, was ",$fcc_row->pi_code," should be $pi_code" if $opt_d;
+    $fcc_row->update({
+      'pi_code' => $pi_code,
+      'last_fcc_lookup' => $sql_now,
+    });
   }
 }
 
