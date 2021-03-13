5bc04edd00079f27fec2cc6bf4f90334abe8df09
Russell Dwarshuis <kb8u_vhf@hotmail.com>
2021-03-13 12:42:26 -0500

    client inno setup installer

--- client/win32/fm/report_file.pl
+++ client/win32/fm/report_file.pl
@@ -16,40 +16,41 @@ use File::Slurp;
 use Try::Tiny;
 use EV;
 
-our ($opt_d,$opt_h,$opt_p,$opt_T,$opt_t);
-getopts('dp:T:t:h');
+our ($opt_d,$opt_h,$opt_p,$opt_P,$opt_t,$opt_T,$opt_u);
+getopts('dhp:P:t:T:u:');
 
-help() if $opt_T =~ /\D/;
+help() if $opt_t =~ /\D/;
 help() if $opt_h;
 help() unless $opt_t;
 my $debug = $opt_d;
-my $file_prefix = $opt_p ? $opt_p : 'C:/SDRSharp/RDSDataLogger/RDSDataLogger-';
+my $file_path = $opt_p ? $opt_p : 'C:/SDRSharp/RDSDataLogger';
+my $file_prefix = $opt_P ? $opt_P : 'RDSDataLogger-';
 my $interval = $opt_T ? $opt_T : 5*60;
+help() if $interval < 300;
+my $spot_url = $opt_u ? $opt_u : 'http://rabbitears.info/tvdx/fm_spot';
 
 
 # prevent child processes from opening a console window
 Win32::SetChildShowWindow(0);
 
-my $spot_url = 'http://www.rabbitears.info:3000/fm_spot';
-#my $spot_url = 'http://www.rabbitears.info/tvdx/fm_spot';
-
 
 # default so first loop will work correctly.
-my $last_report_dt =  DateTime->now(time_zone => DateTime::TimeZone->new(name=>'local'));
+my $last_report_dt =  DateTime->now(time_zone => DateTime::TimeZone->new(name=>'local'))->subtract(seconds => $interval);
 my $last_report_freq = 0;
 my $latest_line_dt = undef;
 my $latest_line_freq = 0;
 
 my $scan = { signal => {}, tuner_key => 0+$opt_t };
 
-my $ev = EV::timer($interval, $interval, sub { 
+my $ev = EV::timer(0, $interval, sub { 
+  say (scalar localtime, " Looking for log files") if $debug;
   my @line;
   # generate today's and yesterday's file names
   my $dt_tz = DateTime::TimeZone->new(name=>'local');
   my $dt_now = DateTime->now(time_zone => $dt_tz);
   my $tz_offset = DateTime::TimeZone->offset_as_string($dt_tz->offset_for_datetime($dt_now));
   $tz_offset =~ s/00$/:00/;
-  my $today_file = $file_prefix . $dt_now->ymd . '.csv';
+  my $today_file = "$file_path/$file_prefix" . $dt_now->ymd . '.csv';
   my $yesterday_file = $file_prefix . $dt_now->subtract(days=>1)->ymd . '.csv';
 
   # slurp yesterday and today file into one
@@ -58,8 +59,14 @@ my $ev = EV::timer($interval, $interval, sub {
   try { push @line, (read_file($today_file)); }
   catch { say "couldn't open $today_file: $_" if $debug; };
 
-  for (my $i = $#line; $i != 0; $i--) {
+  return unless @line;
+
+  for (my $i = $#line; $i >= 0; $i--) {
     my ($time,$frequency,$pi) = split ',', $line[$i]; 
+    unless ($time && $frequency && $pi) {
+      say "invalid line: $line[$i]" if $debug;
+      next;
+    }
     next if ($pi eq 'FFFF' || $pi eq '0000');
     # change to ISO8601 format
     $time =~ s/\//-/g; 
@@ -85,7 +92,7 @@ my $ev = EV::timer($interval, $interval, sub {
   }
 
   # first time run if files have all been read
-  report($scan) if ($scan->{signal});
+  report($scan) if (%{$scan->{signal}});
 });
 
 EV::run;
@@ -128,13 +135,16 @@ to this computer and send detected station identication to a website where the
 results can be viewed on a map.
 
 Program options:
+-d Print debugging information.
 -h Print help (you're reading it)
--p RDS scan file prefix.  Defaults to C:/SDRSharp/RDSDataLogger/RDSDataLogger-
+-p RDS scan file path.  Defaults to C:/SDRSharp/RDSDataLogger
+   use / instead of \ in path names.
+-P RDS file prefix.  Defaults to RDSDataLogger-
+-t Mandatory ID so web site can know what tuner is where.  Contact
+     webmaster@rabbitears.info for an ID number.
 -T send scan results every (this many minutes).  Default 5 minutes.  Minimum
    of 5 minutes.
--t Mandatory ID so web site can know what tuner is where.  Contact
-     kb8u_vhf@hotmail.com for an ID number.
--d Run only one band scan and print debugging information.
+-u URL to send data to (developer may ask you to set this to help debug).
 EOHELP
   exit;
 }
