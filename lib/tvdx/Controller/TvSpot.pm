5807bcb858b5b8d28cc2cc09cafb063b2c003bb0
Russell Dwarshuis <kb8u_vhf@hotmail.com>
2026-05-05 14:26:20 -0400

    successfully writes l1detail

--- lib/tvdx/Controller/TvSpot.pm
+++ lib/tvdx/Controller/TvSpot.pm
@@ -545,7 +545,7 @@ ISQL
   # loop over json and append to $sql
   for my $channel (keys %{$args->{json}{rf_channel}}) {
     my $ch = $args->{json}{rf_channel}{$channel};
-    $sql .= '(?,?,?,?,?,?,?,?,?,?),';
+    $sql .= '(?,?,?,?,?,?,?,?,?,?,?),';
     push @vals,($args->{mysql_now},$args->{mysql_now},$channel,$ch->{modulation},$ch->{strength});
     push @vals,($ch->{sig_noise},$args->{tuner_id},$args->{tuner_number});
     push @vals, (defined $ch->{found_call} ? $ch->{found_call} : 'none');
@@ -553,7 +553,7 @@ ISQL
     push @vals, (defined $ch->{l1detail} ? $ch->{l1detail} : undef);
   }
   chop $sql;  # remove , from last row
-  $sql .= " on duplicate key update rx_date='$args->{mysql_now}',strength=values(strength),sig_noise=values(sig_noise),virtual_channel=values(virtual_channel);";
+  $sql .= " on duplicate key update rx_date='$args->{mysql_now}',strength=values(strength),sig_noise=values(sig_noise),virtual_channel=values(virtual_channel),l1detail=values(l1detail);";
 
   $storage->dbh_do(sub {my ($s,$dbh,@args) =@_; my $sth = $dbh->prepare($sql); $sth->execute(@vals)});
 
